73b4b58c4eeb8510acad1233ba9a76d1fb9e46de
Daniel Lévy Bureau <daniel.levy@cerema.fr>
2018-08-08 17:56:56 +0200

    COMMIT_&_PUSH_DL_BUREAU

---
+++ src/main/java/levy/daniel/application/vues/desktop/metier/regex/saisietexte/SaisieTexteVueFxml.java
@@ -0,0 +1,59 @@
+package levy.daniel.application.vues.desktop.metier.regex.saisietexte;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * CLASSE SaisieTexteVueFxml :<br/>
+ * prépare la VUE AnchorPane <b>this.saisieTexteAnchorPane</b> 
+ * modélisant le panneau de saisie d'un texte.<br/>
+ * <ul>
+ * <li>charge dans sa méthode dessiner() le FXML.</li>
+ * <li></li>
+ * <li></li>
+ * <li></li>
+ * </ul>
+ * <br/>
+ *
+ * - Exemple d'utilisation :<br/>
+ *<br/>
+ * 
+ * - Mots-clé :<br/>
+ * <br/>
+ *
+ * - Dépendances :<br/>
+ * <br/>
+ *
+ *
+ * @author daniel.levy Lévy
+ * @version 1.0
+ * @since 8 août 2018
+ *
+ */
+public class SaisieTexteVueFxml {
+
+	// ************************ATTRIBUTS************************************/
+
+	/**
+	 * personneAnchorPane : AnchorPane :<br/>
+	 * panneau AnchorPane (VUE) modélisant un 
+	 * panneau de saisie d'un texte.<br/>
+	 */
+	private AnchorPane saisieTexteAnchorPane;
+
+	
+	/**
+	 * LOG : Log : 
+	 * Logger pour Log4j (utilisant commons-logging).
+	 */
+	private static final Log LOG 
+		= LogFactory.getLog(SaisieTexteVueFxml.class);
+
+	// *************************METHODES************************************/
+	
+	
+	
+
+} // FIN DE LA CLASSE SaisieTexteVueFxml.------------------------------------
