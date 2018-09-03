d615b055b24f88ec1c22c8431071faf2b67eecec
daniel.levy <daniel.levy@cerema.fr>
2018-09-03 09:22:31 +0200

    COMMIT_DL_BUREAU

--- src/main/java/levy/daniel/application/package-info.java
+++ src/main/java/levy/daniel/application/package-info.java
@@ -2,6 +2,35 @@
  * CLASSE package-info :<br/>
  * Ce package contient toutes les classes source de l'application.<br/>
  * <br/>
+ * <ul>
+ * <li>
+ * Ce package <b>levy.daniel.application</b> correspond au 
+ * <b>GroupId</b> dans MAVEN.
+ * </li>
+ * <li>
+ * L'architecture applicative retenue est de type SOA 
+ * (Service Oriented Architecture = Architecture Orientée Services).
+ * </li>
+ * <br/>
+ * <li>
+ * <img src="../../../../../../javadoc/images/architecture_applicative.png" 
+ * alt="architecture applicative" border="1" align="center" />
+ * </li>
+ * <br/>
+ * <li>
+ * L'arborescence du projet est la suivante :</li>
+ * </li>
+ * <br/>
+ * <li>
+ * <img src="../../../../../../javadoc/images/arborescence.png" 
+ * alt="arborescence" border="1" align="center" />
+ * </li>
+ * </ul>
+ * 
+ * <br/><br/>
+ * <img src="../../../../../../javadoc/images/arborescence_model.png" 
+ * alt="arborescence du projet" border="1" align="center" />
+ * <br/><br/>
  *
  * - Exemple d'utilisation :<br/>
  *<br/>
